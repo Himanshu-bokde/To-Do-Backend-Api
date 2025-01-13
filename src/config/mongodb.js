ab008c78b923946e573b9bbc52273322b49dddd5
Himanshu Bokde <himanshubokde09@gmail.com>
2025-01-13 21:12:17 +0530

    Change Monitoring log to console log  to save file size

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -7,7 +7,7 @@ const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/mydb";
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI);
-    logger.info("✅ Connected to MongoDB");
+    console.log("✅ Connected to MongoDB");
   } catch (err) {
     logger.error(`❌ MongoDB connection error: ${err.message}`);
     process.exit(1); // Exit process with failure
